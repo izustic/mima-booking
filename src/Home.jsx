9818a0e2947938040308d529b26163cb50c33452
izuchukwu obi <izuchukwuobi997@gmail.com>
2023-10-16 07:35:41 +0100

    set up

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from './components/SideBar'
+import Main from './components/Main'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <SideBar />
+      <Main />
+    </div>
+  )
+}
+
+export default Home
